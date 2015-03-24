ae3afd83908704e5acee7279d58b009c7a482fda
Jesus Alberto Saiz <ja.saizc@alumnos.urjc.es>
2015-03-24 14:40:42 +0100

    Arreglado index.html

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ function supports_history_api() {
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-           "http://http://jasaizc.github.io/X-Nav-5.7.18-HistoryAPI/gallery/" +
+           "http://jasaizc.github.io/X-Nav-5.7.18-HistoryAPI/gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
